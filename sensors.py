e924ea0c55597a838af01b3e303bbe81998ee8f4
Whth <88489697+Whth@users.noreply.github.com>
2023-05-11 07:54:24 +0800

    清除ctypes 冗余声明
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

---
+++ sensors.py
@@ -0,0 +1,9 @@
+from repo.uptechStar.valueTest import read_sensors
+from repo.uptechStar.uptech import UpTech
+
+if __name__ == '__main__':
+    read_sensors(2)
+    # tk = UpTech()
+    # print(tk.gyro_all)
+    # tk.MPU6500_Open()
+    # print(tk.MPU6500_GetGyro()[0])
---
+++ state_machine.py
@@ -0,0 +1,15 @@
+class StateMachine(object):
+
+    def __init__(self):
+        """
+        initialize the state machine
+        """
+
+    def check_state(self, sensors):
+        """
+
+        :param sensors:
+        :return:
+        """
+
+        pass
