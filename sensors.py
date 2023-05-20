324ce999830fb76d085f6c82df711cd67b759f73
Whth <88489697+Whth@users.noreply.github.com>
2023-05-20 12:11:17 +0800

    移动模块
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- sensors.py
+++
@@ -1,9 +0,0 @@
-from repo.uptechStar.hotConfigure.valueTest import read_sensors
-from repo.uptechStar.uptech import UpTech
-
-if __name__ == '__main__':
-    read_sensors(2)
-    # tk = UpTech()
-    # print(tk.gyro_all)
-    # tk.MPU6500_Open()
-    # print(tk.MPU6500_GetGyro()[0])
