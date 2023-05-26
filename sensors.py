b22dbb3c3a80737d6f967027037d81106f79c37e
Whth <88489697+Whth@users.noreply.github.com>
2023-05-26 12:05:03 +0800

    移动
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- sensors.py
+++ sensors.py
@@ -1,4 +1,9 @@
 from repo.uptechStar.hotConfigure.valueTest import read_sensors
+from ctypes import cdll
 
 if __name__ == '__main__':
-    read_sensors(2)
+    # read_sensors(2)
+    # sen = cdll.LoadLibrary(
+    #     '/home/pi/BattleBot/repo/uptechStar/build/uptechStar/hotConfigure/valueTest.cpython-310-arm-linux-gnueabihf.so')
+    read_sensors(1, interval=0.001)
+    # read_sensors(1)
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-from repo.uptechStar.up_controller import motor_speed_test
+# from repo.uptechStar.up_controller import motor_speed_test
+from repo.uptechStar.uptech import UpTech
 
 if __name__ == '__main__':
-    motor_speed_test(laps=1)
-    motor_speed_test(laps=1, using_id=False)
+    # motor_speed_test(laps=1)
+    # motor_speed_test(laps=1, using_id=False)
+    a = UpTech(debug=True, fan_control=False)
+
+    print(a.get_io(0))
