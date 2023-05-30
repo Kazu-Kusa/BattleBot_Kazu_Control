ef3e0348553f9ca3bfacde149f4358504f06c7f5
Whth <88489697+Whth@users.noreply.github.com>
2023-05-31 00:23:24 +0800

    adjust error compute functions
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/algrithm_tools.py
+++ module/algrithm_tools.py
@@ -1,28 +1,16 @@
-def compute_error(current_angle, target_angle, direction=1):
+def compute_error(current_angle: float, target_angle: float, direction=1) -> float:
     """
     计算当前角度与目标角度之间的角度偏差。
     :param current_angle: 当前角度，单位：度数。取值范围 [-180, 180]
     :param target_angle: 目标角度，单位：度数。取值范围 [-180, 180]
     :param direction: 方向，1 表示顺时针，-1 表示逆时针。
     :return: 当前角度到目标角度的角度差，单位：度数，取值范围：[-180, 180]
-
-    :document:
-        可以将当前角度和目标角度以及方向作为参数传入该函数，函数会先计算出绝对角度差，
-        然后根据方向参数来判断应该返回顺时针方向的角度差还是逆时针方向的角度差。
-        如果 direction=1，则表示需要计算顺时针方向的角度差，所以就将绝对角度差取相反数；
-        如果 direction=-1，则表示需要计算逆时针方向的角度差，同样也将绝对角度差取相反数即可。
     """
-    error = (target_angle - current_angle) % 360  # 先计算出绝对角度差
+    angle_diff = (target_angle - current_angle) % 360
+    if angle_diff > 180:
+        angle_diff -= 360
 
-    if error > 180:  # 如果绝对角度差大于 180 度，则表示需要改变方向
-        error = 360 - error
-
-        if direction == 1:
-            error = -error  # 若要计算顺时针方向的角度差，则取相反数
-    elif direction == -1:
-        error = -error  # 若要计算逆时针方向的角度差，则取相反数
-
-    return error
+    return angle_diff * direction
 
 
 def calculate_relative_angle(current_angle: float, offset_angle: float) -> float:
@@ -42,21 +30,10 @@ def determine_direction(current_angle: float, target_angle: float) -> int:
     判断当前角度移动到目标角度是逆时针更近还是顺时针更近。
     :param current_angle: 当前角度，单位：度数。取值范围 [-180, 180]
     :param target_angle: 目标角度，单位：度数。取值范围 [-180, 180]
-    :return: 若逆时针更近，则返回 -1；若顺时针更近，则返回 1；若两者距离相等，则返回 0。
+    :return: 若逆时针更近，则返回 -1；若顺时针更近，则返回 1；若两者距离相等，则返回 -1。
     """
-    # 将角度都转为正值
-    current_angle = (current_angle + 360) % 360
-    target_angle = (target_angle + 360) % 360
-
-    # 计算逆时针距离和顺时针距离
-    counterclockwise_distance = min((360 - target_angle + current_angle) % 360,
-                                    (360 - current_angle + target_angle) % 360)
-    clockwise_distance = min((360 - current_angle + target_angle) % 360, (360 - target_angle + current_angle) % 360)
-
-    # 判断两者距离哪个更短
-    if counterclockwise_distance < clockwise_distance:
-        return -1  # 逆时针更近
-    elif counterclockwise_distance > clockwise_distance:
-        return 1  # 顺时针更近
+    angle_diff = (target_angle - current_angle + 180) % 360 - 180
+    if angle_diff == 0:
+        return -1  # 两者距离相等，按照题目要求返回 -1。
     else:
-        return 0  # 两者距离相等
+        return int(angle_diff / abs(angle_diff))
