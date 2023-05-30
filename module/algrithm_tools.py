75472a2d0b7b071021130f5cb4128713c0ccc643
Whth <88489697+Whth@users.noreply.github.com>
2023-05-31 01:27:25 +0800

    simplify
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- module/algrithm_tools.py
+++ module/algrithm_tools.py
@@ -20,9 +20,7 @@ def calculate_relative_angle(current_angle: float, offset_angle: float) -> float
     :param offset_angle: 偏移角度，单位：度数。
     :return: 偏移后的目标角度，单位：度数。取值范围 [-180, 180]
     """
-    target_angle = current_angle + offset_angle  # 目标角度 = 当前角度 + 偏移角度
-    target_angle = (target_angle + 180) % 360 - 180  # 确保目标角度在 [-180, 180] 的范围内
-    return target_angle
+    return (current_angle + offset_angle + 180) % 360 - 180
 
 
 def determine_direction(current_angle: float, target_angle: float) -> int:
@@ -30,10 +28,7 @@ def determine_direction(current_angle: float, target_angle: float) -> int:
     判断当前角度移动到目标角度是逆时针更近还是顺时针更近。
     :param current_angle: 当前角度，单位：度数。取值范围 [-180, 180]
     :param target_angle: 目标角度，单位：度数。取值范围 [-180, 180]
-    :return: 若逆时针更近，则返回 -1；若顺时针更近，则返回 1；若两者距离相等，则返回 -1。
+    :return: 若逆时针更近，则返回 -1；若顺时针更近，则返回 1；若两者距离相等，会返回-1。
     """
     angle_diff = (target_angle - current_angle + 180) % 360 - 180
-    if angle_diff == 0:
-        return -1  # 两者距离相等，按照题目要求返回 -1。
-    else:
-        return int(angle_diff / abs(angle_diff))
+    return int(angle_diff / abs(angle_diff))
