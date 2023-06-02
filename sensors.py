ffd6cf5b7302f99d8920667522c95b31c2e700d3
Whth <88489697+Whth@users.noreply.github.com>
2023-06-02 16:02:46 +0800

    sensor code
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- sensors.py
+++ sensors.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     # read_sensors(2)
     adc = {0: 'edge_rr', 1: 'edge_fr', 2: 'edge_fl', 3: 'edge_rl', 4: 'fb', 5: 'rb', 6: 'fr', 7: 'r2', 8: 'l2'}
 
-    io = {0: 'r_gray', 1: 'l_gray'}
+    io = {7: 'r_gray', 6: 'l_gray'}
     read_sensors(1, interval=0.001, adc_labels=adc, io_labels=io)
     # read_sensors(1)
