6aebb09b70c564215b5bf663e1085aebc09b4833
Whth <88489697+Whth@users.noreply.github.com>
2023-06-09 19:02:57 +0800

    display
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- sensors.py
+++ sensors.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     adc = {0: 'edge_rr', 1: 'edge_fr', 2: 'edge_fl', 3: 'edge_rl', 4: 'fb', 5: 'rb', 6: 'fr', 7: 'r2', 8: 'l2'}
 
     io = {7: 'r_gray', 6: 'l_gray'}
-    read_sensors(1, interval=0.001, adc_labels=adc, io_labels=io)
+    read_sensors(1, adc_labels=adc, io_labels=io)
     # read_sensors(1)
