ecfb82461cb5c691fb846f19bd9cc910f58c7930
Whth <88489697+Whth@users.noreply.github.com>
2023-07-06 11:32:55 +0800

    add tests
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- cache_test.py
+++ cache_test.py
@@ -3,6 +3,7 @@ import time
 from matplotlib import pyplot as plt
 
 from repo.uptechStar.module.db_tools import persistent_lru_cache
+from repo.uptechStar.module.algrithm_tools import bake_to_cache, performance_evaluate
 
 
 @persistent_lru_cache('cache', maxsize=None)
@@ -12,18 +13,20 @@ def my_function(x):
     return x + 1
 
 
-def learning_curve(multiplier: int = 2, laps: int = 20):
+def learning_curve(function=my_function, multiplier: int = 2, laps: int = 20):
     op_durations_list = []
 
     for _ in range(laps):
         start_time = time.time()
         for i in range(256):
             # 第一次调用会计算并缓存结果
-            print(my_function(random.randint(0, 256 * multiplier)))
+            print(function(random.randint(0, 256 * multiplier)))
         op_durations_list.append(time.time() - start_time)
     plt.plot(range(laps), op_durations_list)
     plt.show()
     # 输出: Calculating... \n 6
 
 
-learning_curve(multiplier=10)
+# learning_curve(multiplier=10)
+# bake_to_cache()
+performance_evaluate()
