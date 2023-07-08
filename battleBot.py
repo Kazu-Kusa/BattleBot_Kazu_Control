b1786b709f0e2e33fb04f0d8bb687b220adf9d47
Whth <88489697+Whth@users.noreply.github.com>
2023-07-08 17:24:43 +0800

    clean ,tweak
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- battleBot.py
+++ battleBot.py
@@ -1,5 +1,5 @@
 import warnings
-from random import randint, choice, random
+from random import choice, random
 from typing import Callable
 
 from modules.AbsSurroundInferrer import AbstractSurroundInferrer
@@ -9,7 +9,12 @@ from modules.bot import Bot
 
 from time import perf_counter_ns
 from repo.uptechStar.module.timer import delay_ms
-from repo.uptechStar.module.actions import new_ActionFrame
+from repo.uptechStar.module.actions import new_ActionFrame, ActionFrame
+from repo.uptechStar.module.uptech import build_watcher
+
+SIDES_SENSOR_ID = (7, 8)
+
+START_MAX_LINE = 1800
 
 
 def check_surrounding_fence(ad_list: list, baseline: int = 5000, conner_baseline: int = 2200) -> int:
@@ -57,6 +62,7 @@ def check_surrounding_fence(ad_list: list, baseline: int = 5000, conner_baseline
 class BattleBot(Bot, AbstractSurroundInferrer, Motion):
 
     # TODO: unbind the surrounding objects detection logic to a new class based on ActionPlayer
+
     def __init__(self, config_path: str):
         super().__init__(config_path=config_path)
 
@@ -74,6 +80,10 @@ class BattleBot(Bot, AbstractSurroundInferrer, Motion):
         self._check_surround_kwargs: dict = {}
 
         self.load_config()
+
+        self._start_watcher = build_watcher(sensor_update=self.sensors.adc_all_channels,
+                                            sensor_id=SIDES_SENSOR_ID,
+                                            max_line=START_MAX_LINE)
 
     def load_config(self):
         """
@@ -94,67 +104,23 @@ class BattleBot(Bot, AbstractSurroundInferrer, Motion):
         self._get_away_from_edge_kwargs = self._on_stage_kwargs.get('get_away_from_edge')
         self._check_surround_kwargs = self._on_stage_kwargs.get('check_surround')
 
-    # region special actions
-    def wait_start(self, baseline: int = 1800, check_interval: int = 50,
-                   dash_time: int = 600, dash_speed: int = 8000) -> None:
+    def wait_start(self, dash_time: int = 600, dash_speed: int = 8000) -> None:
         """
         hold still util the start signal is received
         :param dash_speed:
-        :param check_interval:
         :param dash_time:
-        :param baseline:
         :return:
         """
         tape = [new_ActionFrame(action_speed=dash_speed, action_duration=dash_time),
                 new_ActionFrame()]
         self.screen.ADC_Led_SetColor(0, self.screen.COLOR_BROWN)
-        temp_list = self.sensors.adc_all_channels
-        while temp_list[8] < baseline or temp_list[7] < baseline:
-            print(f'\r##HALT AT {perf_counter_ns()}##', end='')
-            delay_ms(check_interval)
-            temp_list = self.sensors.adc_all_channels
+        warnings.warn('##HALTING##')
+        while self._start_watcher():
+            """
+            HALTING
+            """
         warnings.warn('!!DASH-TIME!!')
         self._player.extend(tape)
-
-    def scan_surround(self, detector: Callable[[], bool],
-                      with_ready: bool = False,
-                      ready_time: int = 300,
-                      with_dash: bool = False,
-                      dash_speed: int = -8000,
-                      dash_time: int = 450,
-                      dash_breaker_func: Callable[[], bool] = None,
-                      dash_breaker_action_func: Callable[[], None] = None,
-                      with_turn: bool = False,
-                      spinning_type=randint(0, 1), spinning_speed: int = 2500, max_duration: int = 3000):
-        """
-        checking the stage direction and make the dash movement accordingly
-        :param ready_time:
-        :param with_ready:
-        :param dash_breaker_action_func:
-        :param dash_breaker_func:
-        :param dash_speed:
-        :param dash_time:
-        :param with_turn:
-        :param detector:
-        :param with_dash:
-        :param spinning_type:
-        :param spinning_speed:
-        :param max_duration:
-        :return:
-        """
-        if with_dash:
-            def dash() -> None:
-                self.action_D(dash_speed=dash_speed, dash_time=dash_time, with_turn=with_turn,
-                              breaker_func=dash_breaker_func,
-                              break_action_func=dash_breaker_action_func,
-                              with_ready=with_ready, ready_time=ready_time)
-        else:
-            dash = (lambda: None)
-        self.action_T(turn_speed=spinning_speed, turn_time=max_duration,
-                      turn_type=spinning_type,
-                      breaker_func=detector, break_action_func=dash)
-
-    # endregion
 
     # region events
     def util_edge(self, using_gray: bool = True, using_edge_sensor: bool = True, edge_baseline: int = 1800,
@@ -201,67 +167,6 @@ class BattleBot(Bot, AbstractSurroundInferrer, Motion):
         elif using_gray:
             gray_check()
 
-    def on_allay_box(self, speed: int = 5000, multiplier: float = 0):
-        """
-        the action that will be executed on the event when encountering allay box
-        :param speed: the desired speed
-        :param multiplier: the desired speed multiplier
-        :return:
-        """
-        self.screen.ADC_Led_SetColor(1, self.screen.COLOR_GREEN)
-        self.action_T(turn_speed=speed, multiplier=multiplier)
-
-    def on_enemy_box(self, speed: int = 6000, multiplier: float = 0):
-        """
-        the action that will be executed on the event when encountering enemy box
-        :param speed:
-        :param multiplier:
-        :return:
-        """
-
-        self.screen.ADC_Led_SetColor(1, self.screen.COLOR_RED)
-        if multiplier:
-            speed = int(multiplier * speed)
-        self.controller.move_cmd(speed, speed)
-        self.util_edge()
-        self.controller.move_cmd(-speed, -speed)
-        delay_ms(160)
-        self.controller.move_cmd(0, 0)
-
-    def on_enemy_car(self, speed: int = 8000, multiplier: float = 0):
-        """
-        the action that will be executed on the event when encountering enemy car
-        :param speed:
-        :param multiplier:
-        :return:
-        """
-
-        self.screen.ADC_Led_SetColor(1, self.screen.COLOR_RED)
-        if multiplier:
-            speed = int(multiplier * speed)
-        self.controller.move_cmd(speed, speed)
-        self.util_edge()
-        if multiplier:
-            speed = int(multiplier * speed)
-        self.controller.move_cmd(-speed, -speed)
-        delay_ms(160)
-        self.controller.move_cmd(0, 0)
-
-    def on_thing_surrounding(self, position_type: int = 0, rotate_time: int = 160, rotate_speed: int = 3800):
-        """
-        0 for left
-        1 for right
-        2 for behind
-        :param rotate_speed:
-        :param rotate_time:
-        :param position_type:
-        :return:
-        """
-        if position_type == 2:
-            self.action_T(turn_speed=rotate_speed, turn_time=rotate_time, multiplier=1.8)
-        else:
-            self.action_T(turn_type=position_type, turn_speed=rotate_speed, turn_time=rotate_time)
-
     def on_attacked(self, position_type: int, counter_back: bool = False,
                     run_away: bool = False,
                     run_speed: int = 5000, run_time: int = 240,
@@ -305,295 +210,6 @@ class BattleBot(Bot, AbstractSurroundInferrer, Motion):
             self.action_TF(fixed_wheel_id=choice([1, 3]), speed=reacting_speed, tf_time=reacting_time)
 
     # endregion
-
-    def get_away_from_edge(self, adc_list: list[int], io_list: list[int],
-                           edge_baseline: int = 1750, min_baseline: int = 1150,
-                           edge_speed_multiplier: float = 3,
-                           high_speed_time: int = 180, turn_time: int = 160) -> bool:
-        """
-        handles the normal edge case using both adc_list and io_list.
-        but well do not do anything if no edge case
-        :param min_baseline:
-        :param high_speed_time:
-        :param turn_time:
-        :param edge_speed_multiplier:
-        :param adc_list:the list of adc devices returns
-        :param io_list:the list of io devices returns
-        :param edge_baseline: the edge_check baseline
-        :return: if encounter the edge
-        """
-        # change the light color to represent current state, state of in normal behave
-        self.screen.ADC_Led_SetColor(0, self.screen.COLOR_BLUE)
-
-        # read the adc sensors
-        edge_rr_sensor = adc_list[0]
-        edge_fr_sensor = adc_list[1]
-        edge_fl_sensor = adc_list[2]
-        edge_rl_sensor = adc_list[3]
-
-        # the closer to the edge ,the slower the wheels rotates
-        high_spead = int(edge_speed_multiplier * min(edge_rl_sensor, edge_fl_sensor, edge_fr_sensor, edge_rr_sensor))
-
-        def rear_watcher() -> bool:
-
-            temp = self.sensors.adc_all_channels
-            local_edge_rr_sensor = temp[0]
-            local_edge_rl_sensor = temp[3]
-            if local_edge_rl_sensor < edge_baseline or local_edge_rr_sensor < edge_baseline:
-                # if at least one of the edge sensor is hanging over air
-                return True
-            else:
-                return False
-
-        def front_watcher() -> bool:
-            temp = self.sensors.adc_all_channels
-            local_edge_fr_sensor = temp[1]
-            local_edge_fl_sensor = temp[2]
-            if local_edge_fl_sensor < edge_baseline or local_edge_fr_sensor < edge_baseline:
-                # if at least one of the edge sensor is hanging over air
-                return True
-            else:
-                return False
-
-        def halt() -> None:
-            self.controller.move_cmd(0, 0)
-
-        def stop() -> bool:
-            warnings.warn('>>>>FLOATING<<<<')
-            halt()
-            return True
-
-        # region methods
-        def do_nothing():
-            return False
-
-        def do_fl():
-            """
-            [fl]         fr
-                O-----O
-                   |
-                O-----O
-            rl           rr
-
-            front-left encounters the edge, turn right,turn type is 1
-            """
-            self.action_BT(back_speed=high_spead, back_time=high_speed_time,
-                           turn_speed=high_spead, turn_time=turn_time,
-                           b_multiplier=1.1,
-                           t_multiplier=0.7, turn_type=1,
-                           hind_watcher_func=rear_watcher)
-            return True
-
-        def do_fr():
-            """
-           fl          [fr]
-               O-----O
-                  |
-               O-----O
-           rl           rr
-
-           front-right encounters the edge, turn left,turn type is 0
-            """
-            self.action_BT(back_speed=high_spead, back_time=high_speed_time,
-                           turn_speed=high_spead, turn_time=turn_time,
-                           b_multiplier=1.1,
-                           t_multiplier=0.7, turn_type=0,
-                           hind_watcher_func=rear_watcher)
-            return True
-
-        def do_rl():
-            """
-            fl           fr
-                O-----O
-                   |
-                O-----O
-            [rl]         rr
-
-            rear-left encounters the edge, turn right,turn type is 1
-            """
-            self.action_T(turn_type=1, turn_speed=high_spead, turn_time=turn_time, multiplier=1.2)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=front_watcher, break_action_func=halt)
-            return True
-
-        def do_rr():
-            """
-            fl           fr
-                O-----O
-                   |
-                O-----O
-            rl          [rr]
-
-            rear-right encounters the edge, turn left,turn type is 0
-            """
-            self.action_T(turn_type=0, turn_speed=high_spead, turn_time=turn_time, multiplier=0.9)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=front_watcher, break_action_func=halt)
-            return True
-
-        def do_fl_rl():
-            """
-             [fl]   l   r   fr
-                 O-----O
-                    |
-                 O-----O
-            [rl]            rr
-            :return:
-            """
-            self.action_T(turn_type=1, turn_speed=high_spead, multiplier=1.2, turn_time=turn_time)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=front_watcher, break_action_func=halt)
-            return True
-
-        def do_fr_rr():
-            """
-              fl   l   r  [fr]
-                 O-----O
-                    |
-                 O-----O
-             rl           [rr]
-            :return:
-            """
-            self.action_T(turn_type=0, turn_speed=high_spead, multiplier=1.2, turn_time=turn_time)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=front_watcher, break_action_func=halt)
-            return True
-
-        def do_rl_rr():
-            """
-             fl   l   r   fr
-                 O-----O
-                    |
-                 O-----O
-            [rl]          [rr]
-            :return:
-            """
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=front_watcher, break_action_func=halt)
-            return True
-
-        def do_fl_fr():
-            """
-             [fl]   l   r   [fr]
-                 O-----O
-                    |
-                 O-----O
-            rl          rr
-            :return:
-            """
-            self.action_BT(back_speed=high_spead, back_time=high_speed_time,
-                           turn_speed=high_spead, turn_time=int(turn_time * 1.3),
-                           b_multiplier=1.1,
-                           t_multiplier=0.7, turn_type=0,
-                           hind_watcher_func=rear_watcher)
-            return True
-
-        def do_fr_rl_rr():
-            """
-             fl   l   r   [fr]
-                 O-----O
-                    |
-                 O-----O
-            [rl]          [rr]
-            :return:
-            """
-            self.action_T(turn_type=0, turn_speed=high_spead, turn_time=turn_time, multiplier=0.3)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=front_watcher, break_action_func=halt)
-            return True
-
-        def do_fl_rl_rr():
-            """
-            [fl]   l   r   fr
-                 O-----O
-                    |
-                 O-----O
-            [rl]          [rr]
-            :return:
-            """
-            self.action_T(turn_type=1, turn_speed=high_spead, turn_time=turn_time, multiplier=0.3)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=front_watcher, break_action_func=halt)
-            return True
-
-        def do_fl_fr_rr():
-            """
-             [fl]   l   r   [fr]
-                 O-----O
-                    |
-                 O-----O
-             rl           [rr]
-            :return:
-            """
-            self.action_T(turn_type=1, turn_speed=high_spead, turn_time=turn_time, multiplier=0.3)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=rear_watcher, break_action_func=halt,
-                          with_turn=True)
-            return True
-
-        def do_fl_fr_rl():
-            """
-             [fl]   l   r   [fr]
-                 O-----O
-                    |
-                 O-----O
-             [rl]            rr
-            :return:
-            """
-            self.action_T(turn_type=0, turn_speed=high_spead, turn_time=turn_time, multiplier=0.3)
-            self.action_D(dash_speed=high_spead, dash_time=high_speed_time, multiplier=1.1,
-                          breaker_func=rear_watcher, break_action_func=halt,
-                          with_turn=True)
-            return True
-
-        # endregion
-
-        sensor_data = (edge_fl_sensor > edge_baseline and edge_fl_sensor > min_baseline,
-                       edge_fr_sensor > edge_baseline and edge_fr_sensor > min_baseline,
-                       edge_rl_sensor > edge_baseline and edge_rl_sensor > min_baseline,
-                       edge_rr_sensor > edge_baseline and edge_rr_sensor > min_baseline)
-
-        method_table = {(True, True, True, True): do_nothing,
-                        (False, False, False, False): stop,
-                        # region one edge sensor only
-                        (False, True, True, True): do_fl,
-                        (True, False, True, True): do_fr,
-                        (True, True, False, True): do_rl,
-                        (True, True, True, False): do_rr,
-                        # endregion
-
-                        # region double edge sensor only
-                        (False, True, False, True): do_fl_rl,  # normal
-                        (True, False, True, False): do_fr_rr,
-                        (True, True, False, False): do_rl_rr,
-                        (False, False, True, True): do_fl_fr,
-
-                        # (True, False, False, True): do_rl_rr, #abnormal such case are hard to be classified
-                        # (False, True, True, False): do_rl_rr,
-                        # endregion
-
-                        # region triple edge sensor
-                        (True, False, False, False): do_fr_rl_rr,  # specified in conner
-                        (False, True, False, False): do_fl_rl_rr,
-                        (False, False, True, False): do_fl_fr_rr,
-                        (False, False, False, True): do_fl_fr_rl,
-                        # endregion
-                        (1, 1): do_nothing,
-                        (0, 1): do_fl,
-                        (1, 0): do_fr,
-                        (0, 0): do_fl_fr
-                        }
-
-        method = method_table.get(sensor_data)
-        if method:
-            return method()
-        else:
-            # read the io sensors
-            l_gray = io_list[6]
-            r_gray = io_list[7]
-            gray_sensor_data = (l_gray, r_gray)
-
-            return method_table.get(gray_sensor_data)
 
     def check_surround(self, adc_list: list[int], baseline: int = 2000, basic_speed: int = 6000,
                        evade_prob: float = 0.1) -> bool:
