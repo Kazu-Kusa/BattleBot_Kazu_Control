a91171fac012894020b02e02cf5e94dd7bc0f2f9
Whth <88489697+Whth@users.noreply.github.com>
2023-07-18 03:18:42 +0800

    :enhance let the inferrer fit the standards
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- modules/AbsSurroundInferrer.py
+++ modules/AbsSurroundInferrer.py
@@ -1,97 +1,102 @@
 from abc import ABCMeta, abstractmethod
+from typing import final, Dict, List, Callable, Tuple, Optional
+
+KEY_BEHIND_OBJECT = 5
+
+KEY_RIGHT_OBJECT = 4
+
+KET_LEFT_OBJECT = 3
+
+KEY_FRONT_ENEMY_CAR = 2
+
+KEY_FRONT_ENEMY_BOX = 1
+
+KEY_FRONT_ALLY_BOX = 0
 
 
 class AbstractSurroundInferrer(metaclass=ABCMeta):
-    @abstractmethod
-    def scan_surround(self, detector, with_ready, ready_time, with_dash, dash_speed, dash_time, dash_breaker_func,
-                      dash_breaker_action_func, with_turn, spinning_type, spinning_speed, max_duration):
-        """
-        checking the stage direction and make the dash movement accordingly
-        :param ready_time:
-        :param with_ready:
-        :param dash_breaker_action_func:
-        :param dash_breaker_func:
-        :param dash_speed:
-        :param dash_time:
-        :param with_turn:
-        :param detector:
-        :param with_dash:
-        :param spinning_type:
-        :param spinning_speed:
-        :param max_duration:
-        :return:
-        """
-        pass
 
     @abstractmethod
-    def on_allay_box(self, speed, multiplier):
+    def on_allay_box_at_front(self, basic_speed, multiplier):
         """
         the action that will be executed on the event when encountering allay box
-        :param speed: the desired speed
+        :param basic_speed: the desired speed
         :param multiplier: the desired speed multiplier
         :return:
         """
         pass
 
     @abstractmethod
-    def on_enemy_box(self, speed, multiplier):
+    def on_enemy_box_at_front(self, basic_speed, multiplier):
         """
         the action that will be executed on the event when encountering enemy box
-        :param speed:
+        :param basic_speed:
         :param multiplier:
         :return:
         """
         pass
 
     @abstractmethod
-    def on_enemy_car(self, speed, multiplier):
+    def on_enemy_car_at_front(self, basic_speed, multiplier):
         """
         the action that will be executed on the event when encountering enemy car
-        :param speed:
+        :param basic_speed:
         :param multiplier:
         :return:
         """
         pass
 
     @abstractmethod
-    def on_thing_surrounding(self, position_type, rotate_time, rotate_speed):
+    def on_object_encountered_at_left(self, basic_speed, multiplier):
         """
-        0 for left
-        1 for right
-        2 for behind
-        :param rotate_speed:
-        :param rotate_time:
-        :param position_type:
-        :return:
-        """
-        pass
-
-    @abstractmethod
-    def on_attacked(self, position_type, counter_back, run_away, run_speed, run_time, run_away_breaker_func,
-                    run_away_break_action_func, reacting_speed, reacting_time):
-        """
-        use action tf to evade attacks
-        :param run_away_break_action_func:
-        :param run_away_breaker_func:
-        :param run_time:
-        :param run_away:
-        :param run_speed:
-        :param counter_back:
-        :param position_type:
-        :param reacting_speed:
-        :param reacting_time:
-        :return:
-        """
-        pass
-
-    @abstractmethod
-    def check_surround(self, adc_list, baseline, basic_speed, evade_prob):
-        """
-        checks sensors to get surrounding objects
-        :param evade_prob:
+        the action that will be executed on the event when encountering object at left
         :param basic_speed:
+        :param multiplier:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def on_object_encountered_at_right(self, basic_speed, multiplier):
+        """
+        the action that will be executed on the event when encountering object at right
+        :param basic_speed:
+        :param multiplier:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def on_object_encountered_at_behind(self, basic_speed, multiplier):
+        """
+        the action that will be executed on the event when encountering object at behind
+        :param basic_speed:
+        :param multiplier:
+        :return:
+        """
+        pass
+
+    @abstractmethod
+    def evaluate_surrounding_status(self, adc_list) -> int:
+        """
+        checks sensors to get surrounding status code
         :param adc_list:
-        :param baseline:
         :return: if it has encountered anything
         """
         pass
+
+    __method_table: Dict[int, Callable] = {
+        KEY_FRONT_ALLY_BOX: on_allay_box_at_front,
+        KEY_FRONT_ENEMY_BOX: on_enemy_box_at_front,
+        KEY_FRONT_ENEMY_CAR: on_enemy_car_at_front,
+        KET_LEFT_OBJECT: on_object_encountered_at_left,
+        KEY_RIGHT_OBJECT: on_object_encountered_at_right,
+        KEY_BEHIND_OBJECT: on_object_encountered_at_behind
+    }
+
+    @final
+    def react_to_surroundings(self, adc_list, speed, multiplier) -> int:
+        status_code = self.evaluate_surrounding_status(adc_list)
+        method: Callable = self.__method_table.get(status_code)
+        method(speed, multiplier)
+        return status_code
