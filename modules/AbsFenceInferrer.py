78e72faf9bd05699c246caf958616f9d406dcabd
Whth <88489697+Whth@users.noreply.github.com>
2023-07-25 02:56:56 +0800

    :adjust adapts
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- modules/AbsFenceInferrer.py
+++ modules/AbsFenceInferrer.py
@@ -1,25 +1,33 @@
 from abc import ABCMeta, abstractmethod
-from typing import final, Dict, List, Callable, Tuple, Optional
+from typing import final, Dict, List, Callable, Tuple, Optional, Hashable, Sequence, Union, Any
 
 from repo.uptechStar.module.actions import ActionPlayer
+from repo.uptechStar.module.inferrer_base import InferrerBase, Reaction, ComplexAction
 
 
-class AbstractFenceInferrer(metaclass=ABCMeta):
-    KEY_FRONT_TO_FENCE = 0
-    KEY_LEFT_TO_FENCE = 1
-    KEY_RIGHT_TO_FENCE = 2
-    KEY_BEHIND_TO_FENCE = 3
-    KEY_FRONT_LEFT_TO_FENCE = 4
-    KEY_FRONT_RIGHT_TO_FENCE = 5
-    KEY_BEHIND_LEFT_TO_FENCE = 6
-    KEY_BEHIND_RIGHT_TO_FENCE = 7
+class AbstractFenceInferrer(InferrerBase):
+    KEY_FRONT_TO_FENCE = 1
+    KEY_LEFT_TO_FENCE = 2
+    KEY_RIGHT_TO_FENCE = 4
+    KEY_BEHIND_TO_FENCE = 8
+    KEY_FRONT_LEFT_TO_FENCE = KEY_FRONT_TO_FENCE + KEY_LEFT_TO_FENCE  # code: 3
+    KEY_FRONT_RIGHT_TO_FENCE = KEY_FRONT_TO_FENCE + KEY_RIGHT_TO_FENCE  # code: 5
+    KEY_BEHIND_LEFT_TO_FENCE = KEY_BEHIND_TO_FENCE + KEY_LEFT_TO_FENCE  # code: 7
+    KEY_BEHIND_RIGHT_TO_FENCE = KEY_BEHIND_TO_FENCE + KEY_RIGHT_TO_FENCE  # code: 9
 
-    def __init__(self, basic_duration: int, player: ActionPlayer):
-        self._basic_duration = basic_duration
-        self._player = player
+    def _action_table_init(self):
+        self.register_action(case=self.KEY_FRONT_TO_FENCE, complex_action=self.on_front_to_fence)
+        self.register_action(case=self.KEY_LEFT_TO_FENCE, complex_action=self.on_left_to_fence)
+        self.register_action(case=self.KEY_RIGHT_TO_FENCE, complex_action=self.on_right_to_fence)
+        self.register_action(case=self.KEY_BEHIND_TO_FENCE, complex_action=self.on_behind_to_fence)
+        self.register_action(case=self.KEY_FRONT_LEFT_TO_FENCE, complex_action=self.on_front_left_to_fence)
+        self.register_action(case=self.KEY_FRONT_RIGHT_TO_FENCE, complex_action=self.on_front_right_to_fence)
+        self.register_action(case=self.KEY_BEHIND_LEFT_TO_FENCE, complex_action=self.on_behind_left_to_fence)
+        self.register_action(case=self.KEY_BEHIND_RIGHT_TO_FENCE, complex_action=self.on_behind_right_to_fence)
 
+    # region methods
     @abstractmethod
-    def on_front_to_fence(self, basic_speed, multiplier):
+    def on_front_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at front
         :param basic_speed: the desired speed
@@ -29,7 +37,7 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def on_left_to_fence(self, basic_speed, multiplier):
+    def on_left_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at left
         :param basic_speed: the desired speed
@@ -39,7 +47,7 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def on_right_to_fence(self, basic_speed, multiplier):
+    def on_right_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at right
         :param basic_speed: the desired speed
@@ -49,7 +57,7 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def on_behind_to_fence(self, basic_speed, multiplier):
+    def on_behind_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at behind
         :param basic_speed: the desired speed
@@ -59,7 +67,7 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def on_front_left_to_fence(self, basic_speed, multiplier):
+    def on_front_left_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at front left
         :param basic_speed: the desired speed
@@ -69,7 +77,7 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def on_front_right_to_fence(self, basic_speed, multiplier):
+    def on_front_right_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at front right
         :param basic_speed: the desired speed
@@ -79,7 +87,7 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def on_behind_left_to_fence(self, basic_speed, multiplier):
+    def on_behind_left_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at behind left
         :param basic_speed: the desired speed
@@ -89,7 +97,7 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def on_behind_right_to_fence(self, basic_speed, multiplier):
+    def on_behind_right_to_fence(self, basic_speed) -> ComplexAction:
         """
         the action that will be executed on the event when encountering fence at behind right
         :param basic_speed: the desired speed
@@ -98,24 +106,11 @@ class AbstractFenceInferrer(metaclass=ABCMeta):
         """
         pass
 
-    __method_table: Dict[int, Callable] = {
-        KEY_FRONT_TO_FENCE: on_front_to_fence,
-        KEY_LEFT_TO_FENCE: on_left_to_fence,
-        KEY_RIGHT_TO_FENCE: on_right_to_fence,
-        KEY_BEHIND_TO_FENCE: on_behind_to_fence,
-        KEY_FRONT_LEFT_TO_FENCE: on_front_left_to_fence,
-        KEY_FRONT_RIGHT_TO_FENCE: on_front_right_to_fence,
-        KEY_BEHIND_LEFT_TO_FENCE: on_behind_left_to_fence,
-        KEY_BEHIND_RIGHT_TO_FENCE: on_behind_right_to_fence
-    }
+    # endregion
 
     @abstractmethod
-    def evaluate_fence_status(self, adc_list) -> int:
-        pass
+    def infer(self, sensors_data: Sequence[Union[float, int]]) -> Tuple[Hashable, ...]:
+        raise NotImplementedError
 
-    @final
-    def react_to_fence(self, adc_list, basic_speed, multiplier) -> int:
-        status_code = self.evaluate_fence_status(adc_list)
-        method: Callable = self.__method_table.get(status_code)
-        method(basic_speed, multiplier)
-        return status_code
+    def exc_action(self, reaction: Reaction, *args, **kwargs) -> Any:
+        self._player.override(reaction(*args, **kwargs))
