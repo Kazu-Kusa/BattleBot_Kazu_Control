f9d4f71a90e35c4d9f1c01c66cd4fa3f8333cc7f
Whth <88489697+Whth@users.noreply.github.com>
2023-07-25 17:00:53 +0800

    :adjust adapts
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- modules/SurroundInferrers.py
+++ modules/SurroundInferrers.py
@@ -1,12 +1,21 @@
 from modules.AbsSurroundInferrer import AbstractSurroundInferrer
 from repo.uptechStar.module.actions import new_ActionFrame, ActionPlayer
+from repo.uptechStar.module.inferrer_base import ComplexAction
 from repo.uptechStar.module.sensors import SensorHub
 from repo.uptechStar.module.watcher import default_edge_rear_watcher, default_edge_front_watcher, Watcher
 from repo.uptechStar.module.algrithm_tools import random_sign, random_enlarge_multiplier, random_float_multiplier
-from typing import final
+from typing import final, Tuple, Hashable
 
 
 class StandardSurroundInferrer(AbstractSurroundInferrer):
+    def infer(self, *args, **kwargs) -> Tuple[Hashable, ...]:
+        raise NotImplementedError
+
+    CONFIG_BASIC_DURATION_KEY = 'BasicDuration'
+
+    def register_all_config(self):
+        self.register_config(config_registry_path=self.CONFIG_BASIC_DURATION_KEY,
+                             value=1500)
 
     def __init__(self, sensor_hub: SensorHub, action_player: ActionPlayer, config_path: str):
         super().__init__(sensor_hub=sensor_hub, player=action_player, config_path=config_path)
@@ -15,78 +24,68 @@ class StandardSurroundInferrer(AbstractSurroundInferrer):
         self._front_watcher: Watcher = default_edge_front_watcher
 
     @final
-    def on_allay_box_encountered_at_front(self, basic_speed, multiplier):
+    def on_allay_box_encountered_at_front(self, basic_speed: int) -> ComplexAction:
         sign = random_sign()
-        tape = [new_ActionFrame(action_speed=-basic_speed,
+        return [new_ActionFrame(action_speed=-basic_speed,
                                 action_speed_multiplier=random_float_multiplier(),
-                                action_duration=self._basic_duration,
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY),
                                 breaker_func=default_edge_rear_watcher),
                 new_ActionFrame(),
                 new_ActionFrame(action_speed=(sign * basic_speed, -sign * basic_speed),
                                 action_speed_multiplier=random_enlarge_multiplier(),
-                                action_duration=self._basic_duration),
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY)),
                 new_ActionFrame()]
-        self._player.extend(tape)
 
     @final
-    def on_enemy_box_encountered_at_front(self, basic_speed, multiplier):
+    def on_enemy_box_encountered_at_front(self, basic_speed: int) -> ComplexAction:
         sign = random_sign()
-        tape = [new_ActionFrame(action_speed=basic_speed,
+        return [new_ActionFrame(action_speed=basic_speed,
                                 action_speed_multiplier=random_enlarge_multiplier(),
                                 action_duration=6000,
                                 breaker_func=default_edge_front_watcher),
                 new_ActionFrame(),
                 new_ActionFrame(action_speed=-basic_speed,
                                 action_speed_multiplier=random_float_multiplier(),
-                                action_duration=self._basic_duration,
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY),
                                 breaker_func=default_edge_rear_watcher),
                 new_ActionFrame(),
                 new_ActionFrame(action_speed=(sign * basic_speed, -sign * basic_speed),
                                 action_speed_multiplier=random_enlarge_multiplier(),
-                                action_duration=self._basic_duration),
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY)),
                 new_ActionFrame()]
-        self._player.extend(tape)
 
     @final
-    def on_enemy_car_encountered_at_front(self, basic_speed, multiplier):
-        tape = [new_ActionFrame(action_speed=basic_speed,
-                                action_speed_multiplier=random_enlarge_multiplier(),
-                                action_duration=6000,
-                                breaker_func=default_edge_front_watcher),
-                new_ActionFrame(),
-                new_ActionFrame(action_speed=-basic_speed,
-                                action_speed_multiplier=random_float_multiplier(),
-                                action_duration=self._basic_duration,
-                                breaker_func=default_edge_rear_watcher),
-                new_ActionFrame()]
+    def on_enemy_car_encountered_at_front(self, basic_speed: int) -> ComplexAction:
         # TODO: dash til the edge, then fall back,should add a block skill(?)
-        self._player.extend(tape)
-
-    @final
-    def on_object_encountered_at_left(self, basic_speed, multiplier):
-        tape = [new_ActionFrame(action_speed=(-basic_speed, basic_speed),
+        return [new_ActionFrame(action_speed=basic_speed,
                                 action_speed_multiplier=random_enlarge_multiplier(),
-                                action_duration=self._basic_duration),
+                                action_duration=6000,
+                                breaker_func=default_edge_front_watcher),
+                new_ActionFrame(),
+                new_ActionFrame(action_speed=-basic_speed,
+                                action_speed_multiplier=random_float_multiplier(),
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY),
+                                breaker_func=default_edge_rear_watcher),
                 new_ActionFrame()]
-        self._player.extend(tape)
 
     @final
-    def on_object_encountered_at_right(self, basic_speed, multiplier):
-        tape = [new_ActionFrame(action_speed=(basic_speed, -basic_speed),
+    def on_object_encountered_at_left(self, basic_speed: int) -> ComplexAction:
+        return [new_ActionFrame(action_speed=(-basic_speed, basic_speed),
                                 action_speed_multiplier=random_enlarge_multiplier(),
-                                action_duration=self._basic_duration),
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY)),
                 new_ActionFrame()]
-        self._player.extend(tape)
 
     @final
-    def on_object_encountered_at_behind(self, basic_speed, multiplier):
+    def on_object_encountered_at_right(self, basic_speed: int) -> ComplexAction:
+        return [new_ActionFrame(action_speed=(basic_speed, -basic_speed),
+                                action_speed_multiplier=random_enlarge_multiplier(),
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY)),
+                new_ActionFrame()]
+
+    @final
+    def on_object_encountered_at_behind(self, basic_speed: int) -> ComplexAction:
         sign = random_sign()
-        tape = [new_ActionFrame(action_speed=(sign * basic_speed, -sign * basic_speed),
+        return [new_ActionFrame(action_speed=(sign * basic_speed, -sign * basic_speed),
                                 action_speed_multiplier=random_enlarge_multiplier(),
-                                action_duration=self._basic_duration),
+                                action_duration=getattr(self, self.CONFIG_BASIC_DURATION_KEY)),
                 new_ActionFrame()]
-        self._player.extend(tape)
-
-    @final
-    def evaluate_surrounding_status(self, adc_list) -> int:
-        pass
