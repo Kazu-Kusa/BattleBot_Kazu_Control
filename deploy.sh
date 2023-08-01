497fe5d8b429fc4d559af5f39cda178a9f2ab639
Whth <88489697+Whth@users.noreply.github.com>
2023-08-01 15:08:55 +0800

    :feature add opreation chech
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
+# apt source
+APT_FILE_PATH=/etc/apt/sources.list
+sudo echo "deb https://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi" > APT_FILE_PATH
+sudo echo "deb-src https://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ bullseye main non-free contrib rpi" >> APT_FILE_PATH
 
-
-# 替换apt源为清华源（bullseye版本）
-sudo sed -i 's|deb http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi|#deb http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi|g' /etc/apt/sources.list
-sudo sed -i 's|#deb-src http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi|deb-src http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi|g' /etc/apt/sources.list
-sudo sed -i 's|#deb http://archive.raspberrypi.org/debian/ bullseye main|deb http://archive.raspberrypi.org/debian/ bullseye main|g' /etc/apt/sources.list
-sudo sed -i 's|#deb-src http://archive.raspberrypi.org/debian/ bullseye main|deb-src http://archive.raspberrypi.org/debian/ bullseye main|g' /etc/apt/sources.list
-
-# 更新apt包管理器，并替换为清华源
+# apt update
 sudo apt update
 sudo apt upgrade -y
 
-# 安装编译python所需的依赖
-sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev \
-libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev
-
-# 下载并编译安装Python 3.11
-wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tar.xz
+# install python3.11 compile dep
+sudo apt install -y build-essential libffi-dev
+# install python3.11
+wget https://mirrors.huaweicloud.com/python/3.11.0/Python-3.11.0.tar.xz
 tar -xf Python-3.11.0.tar.xz
 cd Python-3.11.0
 ./configure --enable-optimizations
 make -j4
 sudo make install
-
-# 更新pip，并替换为中科大源
+# update pip
 pip3 install --upgrade pip
-sudo pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-
-# 配置raspi-config
+sudo pip3 config set global.index-url https://pypi.mirrors.ustc.edu.cn/simple
+# raspi-config
 sudo raspi-config nonint do_i2c 0  # 激活I2C
 sudo raspi-config nonint do_fan 0  # 激活风扇
 sudo raspi-config nonint set_config_var dtoverlay=gpio-fan,gpiopin=18,temp=60000 /boot/config.txt  # 设置风扇GPIO和激活温度为65℃
-# 开启SPI
+# open SPI
 sudo raspi-config nonint do_spi 0
+# over clock
+if grep -q "arm_freq=2000" /boot/config.txt; then
+    sudo echo "arm_freq=2000" >> /boot/config.txt
+fi
 
-# 设置内核超频
-sudo sed -i 's/#arm_freq=800/arm_freq=2000/g' /boot/config.txt
-sudo sed -i 's/#over_voltage=0/over_voltage=10/g' /boot/config.txt
-sudo sed -i 's/#arm_freq_min=600/arm_freq_min=750/g' /boot/config.txt
+if grep -q "over_voltage=0" /boot/config.txt; then
+    sudo echo "over_voltage=9" >> /boot/config.txt
+fi
 
-# 创建文档并写入配置参数
-echo "APT源已替换为清华源" > environment_config.txt
-echo "Python版本：3.11.0" >> environment_config.txt
-echo "PIP源已替换为中科大源" >> environment_config.txt
-echo "树莓派配置已完成" >> environment_config.txt
-echo "SPI已开启" >> environment_config.txt
-echo "内核超频：ARM频率2000MHz，内核频率750MHz" >> environment_config.txt
+if grep -q "core_freq=750" /boot/config.txt; then
+    sudo echo "core_freq=750" >> /boot/config.txt
+fi
+
