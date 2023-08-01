287c2291ce52804e7a22dbd8b060ce368c835975
Whth <88489697+Whth@users.noreply.github.com>
2023-08-01 17:46:15 +0800

    :feature install python with version check and important modules check
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -8,18 +8,58 @@ sudo sh -c "echo 'deb-src https://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian
 sudo apt update
 sudo apt upgrade -y
 
+python_version="3.11"  # 要判断的Python版本
+
+function installPython(){
 # install python3.11 compile dep
-sudo apt install -y build-essential libffi-dev
-# install python3.11
-wget https://mirrors.huaweicloud.com/python/3.11.0/Python-3.11.0.tar.xz
-tar -xf Python-3.11.0.tar.xz
-cd Python-3.11.0
-./configure --enable-optimizations
-make -j4
-sudo make install
-# update pip
-pip3 install --upgrade pip
-sudo pip3 config set global.index-url https://pypi.mirrors.ustc.edu.cn/simple
+    sudo apt install -y build-essential libffi-dev libssl-dev openssl
+    # install python3.11
+    wget https://mirrors.huaweicloud.com/python/3.11.0/Python-3.11.0.tar.xz
+    tar -xf Python-3.11.0.tar.xz
+    cd Python-3.11.0
+    ./configure --enable-optimizations
+    make -j4
+    sudo make install
+    # update pip
+    pip3 install --upgrade pip
+    sudo pip3 config set global.index-url https://pypi.mirrors.ustc.edu.cn/simple
+
+}
+
+
+function check_python_modules() {
+    if python3 --version 2>&1 | grep -qF "$python_version"; then
+        echo "Python $python_version 已安装."
+
+        # 检查ssl模块是否存在
+        ssl_module=$(python3 -c "import ssl" 2>&1)
+        if [[ $? -eq 0 ]]; then
+            echo "ssl模块已安装"
+        else
+            echo "ssl模块未安装"
+            installPython
+        fi
+
+        # 检查ctypes模块是否存在
+        ctypes_module=$(python3 -c "import ctypes" 2>&1)
+        if [[ $? -eq 0 ]]; then
+            echo "ctypes模块已安装"
+        else
+            echo "ctypes模块未安装"
+            installPython
+        fi
+    else
+        echo "Python $python_version 未安装."
+        installPython
+    fi
+}
+
+# 调用函数
+check_python_modules
+
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+sudo dpkg -i wiringpi-latest.deb
+
 # raspi-config
 sudo raspi-config nonint do_i2c 0  # 激活I2C
 sudo raspi-config nonint do_fan 0  # 激活风扇
