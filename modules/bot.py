d2964cc91ebc65636d1389b1cfd8d8a6db2ced7e
Whth <88489697+Whth@users.noreply.github.com>
2023-08-11 19:15:01 +0800

    :adapts i2c module changes
    Signed-off-by: Whth <88489697+Whth@users.noreply.github.com>

--- modules/bot.py
+++ modules/bot.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 
 from repo.uptechStar.module.actions import ActionPlayer
 from repo.uptechStar.module.camra import Camera
+from repo.uptechStar.module.i2c import SensorI2CExpansion
 from repo.uptechStar.module.onboardsensors import OnBoardSensors
 from repo.uptechStar.module.os_tools import Configurable
 from repo.uptechStar.module.screen import Screen
@@ -17,11 +18,15 @@ class Bot(Configurable, metaclass=ABCMeta):
     # TODO: do remember add the port to the config file
     player = ActionPlayer()
     __on_board_sensors = OnBoardSensors(debug=False)
-    __i2c_expansion_sensors = SensorsSerialExpansion(port='/dev/i2c-1',
-                                                     serial_config=DEFAULT_I2C_SERIAL_KWARGS)
+    __i2c_expansion_sensors = SensorI2CExpansion(
+        SCL_PIN=6, SDA_PIN=7, speed=100,
+        indexed_setter=__on_board_sensors.set_io_level,
+        indexed_getter=__on_board_sensors.get_io_level,
+        indexed_mode_setter=__on_board_sensors.set_io_mode
+    )
     sensor_hub = SensorHub(on_board_adc_updater=__on_board_sensors.adc_all_channels,
                            on_board_io_updater=__on_board_sensors.io_all_channels,
-                           expansion_adc_updater=__i2c_expansion_sensors.get_all_adc_data)
+                           expansion_adc_updater=__i2c_expansion_sensors.get_all_sensor)
 
     @abstractmethod
     def Battle(self, normal_spead: int, use_cam: bool, team_color: str) -> None:
