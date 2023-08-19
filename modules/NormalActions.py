c9682e0c4c0829f1c89f3c1e35abfa3efa06069b
nianzisi <2812839015@qq.com>
2023-08-19 21:53:40 +0800

    perf(随机动作情况的补全 ):
    补全情况

--- modules/NormalActions.py
+++ modules/NormalActions.py
@@ -1,7 +1,9 @@
 from repo.uptechStar.module.actions import ActionPlayer, new_ActionFrame
+from repo.uptechStar.module.algrithm_tools import random_sign
 from repo.uptechStar.module.inferrer_base import InferrerBase
 from repo.uptechStar.module.sensors import SensorHub, FU_INDEX
 from repo.uptechStar.module.watcher import Watcher, build_watcher_simple
+import random
 
 ACTION_REVOLVE = (100, 100, 0, 100)
 
@@ -9,12 +11,17 @@ ACTION_SPEED = (-7000, -7000, 0, -7000)
 
 
 class NormalActions(InferrerBase):
+    def _action_table_init(self):
+        self.register_action()
+
     CONFIG_MOTION_KEY = "MotionSection"
     # TODO 未进行测试的界限值
     CONFIG_MOTION_MIN_LINE_KEY = f"{CONFIG_MOTION_KEY}/MinLine"
     # TODO 未进行测试的超时时间界限值
     CONFIG_MOTION_REVOLVE_TIME_KEY = f"{CONFIG_MOTION_KEY}/Revolve"
     CONFIG_MOTION_SPEED_TIME_KEY = f"{CONFIG_MOTION_KEY}/Speed"
+    # TODO 某个未测定的数字
+    random_factor_speed = random.randint(2000, 4000)
 
     def register_all_config(self):
         self.register_config(config_registry_path=self.CONFIG_MOTION_MIN_LINE_KEY, value=200)
@@ -26,6 +33,20 @@ class NormalActions(InferrerBase):
         self.view: Watcher = build_watcher_simple(sensor_update=self._sensors.on_board_adc_updater[FU_INDEX],
                                                   sensor_id=(8, 0, 5, 3),
                                                   min_line=getattr(self, self.CONFIG_MOTION_MIN_LINE_KEY))
+        self.surrounding_watcher: Watcher = None
+
+    def random_action(self):
+        return [new_ActionFrame(action_speed=self.random_factor_speed,
+                                action_duration=getattr(self, self.CONFIG_MOTION_REVOLVE_TIME_KEY),
+                                break_func=self.surrounding_watcher),
+                new_ActionFrame()]
+
+    def random_revolve_action(self):
+        single = random_sign()
+        return [new_ActionFrame(action_speed=(self.random_factor_speed * single, -single * self.random_factor_speed),
+                                action_duration=getattr(self, self.CONFIG_MOTION_REVOLVE_TIME_KEY),
+                                break_func=self.surrounding_watcher),
+                new_ActionFrame()]
 
     def revolve_action(self):
         # TODO 该函数未测试
