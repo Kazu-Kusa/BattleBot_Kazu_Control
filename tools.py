55432b81b9c39abae9a22bd3f95e2a6cc3e1078f
Whth <88489697+Whth@users.noreply.github.com>
2023-08-21 23:56:43 +0800

    fix(错误的动作速度):

--- tools.py
+++ tools.py
@@ -1,14 +1,20 @@
-from repo.uptechStar.module.hotConfigure.valueTest import read_sensors
+import copy
+import time
+import warnings
 
-adc_labels = {
-    6: 'EDGE_FL',
-    7: "EDGE_RL",
-    2: 'EDGE_FR',
-    1: 'EDGE_RR',
-    8: 'L1',
-    0: 'R1',
-    3: 'FB', 5: 'RB',
+from repo.uptechStar.module.onboardsensors import OnBoardSensors
 
-}
+a = OnBoardSensors()
+print(a.adc_all_channels()[0])
+result = []
+warnings.warn('ascasv')
+time.sleep(10)
+end = time.time() + 5
+while time.time() < end:
+    result.append(copy.deepcopy(a.adc_all_channels()))
+print('a')
 
-read_sensors(adc_labels=adc_labels)
+for i in result:
+    print(list(i))
+print('asc')
+print(len(result))
